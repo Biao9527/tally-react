9094c001d485b3b9e08252773e9cfec1c3aa64ac
周字彪 <1247821135@qq.com>
2022-02-18 15:26:55 +0800

    完成 404 页面

--- src/views/NoMatch.tsx
+++ src/views/NoMatch.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const NotFound = styled.div`
+  text-align: center;
+  margin: 20px 0;
+  >p{
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
+  >a{
+    padding: 0 4px;
+    font-size: 16px;
+    color: #999999;
+    border-bottom: 1px solid #999999;
+  }
+`
 const NoMatch = () => {
   return (
-    <div>地址有误</div>
+    <NotFound>
+      <p>输入地址有误</p>
+      <Link to="/">返回首页</Link>
+    </NotFound>
   );
 };
 
