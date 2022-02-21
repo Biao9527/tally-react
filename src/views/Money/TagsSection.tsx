a387dc7ad4b001b0b9572ce347486c8d28fd3cc7
周字彪 <1247821135@qq.com>
2022-02-21 15:19:25 +0800

    抽离 uesTags

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
+import {useTags} from '../../useTags';
 
 const Wrapper = styled.section`
   font-size: 14px;
@@ -46,7 +47,7 @@ type Props = {
   onChange: (strings: string[])=>void
 }
 const TagsSection: React.FC<Props> = (props) => {
-  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const {tags,setTags} = useTags()
   const selectedTags = props.value
   const addTag = () => {
     const tagName = window.prompt();
