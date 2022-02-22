bc6093ec6610c8527c8d59ad977ab4f579b2c33d
周字彪 <1247821135@qq.com>
2022-02-22 15:15:20 +0800

    创建 Button 组件

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font-size: 16px;
+  border: none;
+  background: #ff852a;
+  color: #ffffff;
+  height: 40px;
+  padding: 0 14px;
+  border-radius: 4px;
+`;
+
+export {Button}
