949ce3741a8758ff69f5050b4a6ad92549705f5a
周字彪 <1247821135@qq.com>
2022-02-26 14:16:27 +0800

    App样式：max-width为500px

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,26 @@ import Money from 'views/Money';
 import Statistics from 'views/Statistics';
 import Tags from 'views/Tags';
 import {Tag} from './views/Tag';
+import styled from 'styled-components';
 
 
-
+const AppWrapper = styled.div`
+  max-width: 500px;
+  margin: auto;
+`
 export default function App() {
   return (
-    <Router>
-          <Routes>
-            <Route path="/" element={<Navigate to="/money"/>}/>
-            <Route path="/tags" element={<Tags/>}/>
-            <Route path="/tags/:id" element={<Tag/>}/>
-            <Route path="/money" element={<Money/>}/>
-            <Route path="/statistics" element={<Statistics/>}/>
-            <Route path="*" element={<NoMatch/>}/>
-          </Routes>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/money"/>}/>
+          <Route path="/tags" element={<Tags/>}/>
+          <Route path="/tags/:id" element={<Tag/>}/>
+          <Route path="/money" element={<Money/>}/>
+          <Route path="/statistics" element={<Statistics/>}/>
+          <Route path="*" element={<NoMatch/>}/>
+        </Routes>
+      </Router>
+    </AppWrapper>
   );
 }
